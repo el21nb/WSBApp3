02b4cb60e357cacdf55b568ca5c545853c27ae3e
barry <el21nb@leeds.ac.uk>
2024-02-11 15:28:41 +0000

    Work on login. error email address badly formatted

---
+++ app/src/main/java/com/example/wsbapp3/activities/LoginActivity.java
@@ -0,0 +1,88 @@
+package com.example.wsbapp3.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.wsbapp3.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+
+//https://github.com/Tesla-gamer/login-signup-app/blob/master/app/src/main/java/com/example/appproject/LoginActivity.java
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button loginButton;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        loginButton = findViewById(R.id.logIn);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etUsername.toString().trim();
+                String password = etPassword.toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    etUsername.setError("No email provided.");
+                    etUsername.requestFocus();
+                } else if (TextUtils.isEmpty(password)) {
+                    etPassword.setError("No password provided.");
+                    etPassword.requestFocus();
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                checkUserRole();
+
+                                Toast.makeText(LoginActivity.this,"Driver signed in", Toast.LENGTH_SHORT);
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Failed to login: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+
+            }
+        });
+    }
+
+    private void checkUserRole() {
+        firebaseAuth.getCurrentUser().getIdToken(false).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
+            @Override
+            public void onSuccess(GetTokenResult result) {
+                boolean isDriver = (boolean) result.getClaims().get("driver");
+                if (isDriver) {
+                    // Show admin UI.
+                    Toast.makeText(LoginActivity.this,"Driver signed in", Toast.LENGTH_SHORT);
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this,"Not a driver account!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+}
